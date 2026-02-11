b78de35181e30e42c8124fdf4409db8436efff9a
Subhash Bhalerao <subhash.devops.learner@gmail.com>
2026-02-11 23:27:44 +0530

    Add /about endpoint with app information

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ app.get('/health', (req, res) => {
   res.json({ status: 'healthy' });
 });
 
+app.get('/about', (req, res) => {
+  res.json({
+    appName: 'CI/CD Learning Project',
+    version: '1.0.0',
+    author: 'Subhash Bhalerao',
+    description: 'Learning DevOps and CI/CD pipelines',
+    endpoints: {
+      '/': 'Main endpoint',
+      '/health': 'Health check',
+      '/about': 'Application information'
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
